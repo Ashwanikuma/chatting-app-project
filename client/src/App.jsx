66e4a54142f6953eb62b0635e96d7cc9c39c3fe3
ashwani kumar <btechcsesscet@gmail.com>
2024-09-13 19:26:26 +0530

    creating login, group, chat, and home file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 import React, { lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Groups = lazy(() => import("./pages/Groups"));
+const Chat = lazy(() => import("./pages/Chat"));
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<h1>About</h1>} />
+        <Route path="/chat/:chatId" element={<Chat />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/login" element={<Login />} />
+        <Route />
       </Routes>
     </BrowserRouter>
   );
