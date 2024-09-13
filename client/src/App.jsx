1def7cd8bffba825b59a69a198c55b4ee97b11c6
ashwani kumar <btechcsesscet@gmail.com>
2024-09-13 19:17:12 +0530

    lazyLoading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+const Home = lazy(() => import("./pages/Home"));
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<h1>About</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
