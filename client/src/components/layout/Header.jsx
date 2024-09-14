ed52ed323c612c6cf4d35f1c2cf37791f7533fe8
ashwani kumar <btechcsesscet@gmail.com>
2024-09-14 13:47:41 +0530

    setting of lazy loading and suspence

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -6,7 +6,7 @@ import {
   IconButton,
   Tooltip,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { Suspense, lazy, useState } from "react";
 import { orange } from "../constants/color";
 import {
   Menu as MenuIcons,
@@ -17,6 +17,10 @@ import {
   Notifications as NotificationsIcon,
 } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
+
+const SearchDialog = lazy(() => import("../specific/Search"));
+const NotificationsDialog = lazy(() => import("../specific/Notifications"));
+const NewGroupDialog = lazy(() => import("../specific/NewGroup"));
 const Header = () => {
   const navigate = useNavigate();
 
@@ -28,7 +32,7 @@ const Header = () => {
     setIsMobile((prev) => !prev);
   };
   const openSearch = () => {
-    setIsMobile((prev) => !prev);
+    setIsSearch((prev) => !prev);
   };
   const openNewGroup = () => {
     setIsNewGroup((prev) => !prev);
@@ -91,6 +95,21 @@ const Header = () => {
           </Toolbar>
         </AppBar>
       </Box>
+      {isSearch && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <SearchDialog />
+        </Suspense>
+      )}
+      {isNotification && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <NotificationsDialog />
+        </Suspense>
+      )}
+      {isNewGroup && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <NewGroupDialog />
+        </Suspense>
+      )}
     </>
   );
 };
