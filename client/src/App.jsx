db7d6e087c25167959b94ec531f70ad128363c5a
ashwani kumar <btechcsesscet@gmail.com>
2024-09-14 08:16:55 +0530

    completed routing with notfound file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React, { lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectRoute from "./components/auth/ProtectRoute";
+
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const Groups = lazy(() => import("./pages/Groups"));
 const Chat = lazy(() => import("./pages/Chat"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 let user = true;
 
 const App = () => {
@@ -25,7 +27,7 @@ const App = () => {
             </ProtectRoute>
           }
         />
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
