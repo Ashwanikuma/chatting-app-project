4a8f9552aa8dcf024422b74f7270dc72180a8a7f
ashwani kumar <btechcsesscet@gmail.com>
2024-09-14 07:56:48 +0530

    creating ProtectRoute file and declaring user var

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const Groups = lazy(() => import("./pages/Groups"));
 const Chat = lazy(() => import("./pages/Chat"));
+let user = true;
 
 const App = () => {
   return (
