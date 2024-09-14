3f074c839ce6cede5e05f3e4c1878e54abda4297
ashwani kumar <btechcsesscet@gmail.com>
2024-09-14 13:26:13 +0530

    creating different icons

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -19,10 +19,24 @@ const AppLayout = () => (WrappedComponent) => {
           >
             First
           </Grid>
-          <Grid item xs={4} height={"100%"} bgcolor="primary.main">
+          <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"}>
             <WrappedComponent {...props} />
           </Grid>
-          <Grid item xs={4} height={"100%"}>
+          <Grid
+            item
+            md={4}
+            lg={3}
+            sx={{
+              display: {
+                xs: "none",
+                sm: "block",
+              },
+
+              padding: "2rem",
+              bgcolor: "rgba(0,0,0,0.85)",
+            }}
+            height={"100%"}
+          >
             Third
           </Grid>
         </Grid>
--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,7 +1,108 @@
-import React from "react";
-
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  IconButton,
+  Tooltip,
+} from "@mui/material";
+import React, { useState } from "react";
+import { orange } from "../constants/color";
+import {
+  Menu as MenuIcons,
+  Search as SearchIcon,
+  Add as AddIcon,
+  Group as GroupIcon,
+  Logout as LogoutIcon,
+  Notifications as NotificationsIcon,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 const Header = () => {
-  return <div>Header</div>;
+  const navigate = useNavigate();
+
+  const [isMobile, setIsMobile] = useState(false);
+  const [isSearch, setIsSearch] = useState(false);
+  const [isNewGroup, setIsNewGroup] = useState(false);
+  const [isNotification, setIsNotification] = useState(false);
+  const handleMobile = () => {
+    setIsMobile((prev) => !prev);
+  };
+  const openSearch = () => {
+    setIsMobile((prev) => !prev);
+  };
+  const openNewGroup = () => {
+    setIsNewGroup((prev) => !prev);
+  };
+
+  const openNotification = () => {
+    setIsNotification((prev) => !prev);
+  };
+
+  const logoutHangler = () => {
+    console.log("logout");
+  };
+  const navigateToGroup = () => navigate("/groups");
+
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }} height={"4rem"}>
+        <AppBar postion="static" sx={{ bgcolor: orange }}>
+          <Toolbar>
+            <Typography
+              variant="h6"
+              sx={{ display: { xs: "none", sm: "block" } }}
+            >
+              chat app
+            </Typography>
+            <Box sx={{ display: { xs: "block", sm: "none" } }}>
+              <IconButton color="inherit" onClick={handleMobile}>
+                <MenuIcons />
+              </IconButton>
+            </Box>
+            <Box sx={{ flexGrow: 1 }}></Box>
+            <Box>
+              <IconBtn
+                title={"search"}
+                icon={<SearchIcon />}
+                onClick={openSearch}
+              />
+              <IconBtn
+                title={"new Group"}
+                icon={<AddIcon />}
+                onClick={openNewGroup}
+              />
+
+              <IconBtn
+                title={"mangage group"}
+                icon={<GroupIcon />}
+                onClick={navigateToGroup}
+              />
+              <IconBtn
+                title={"Notifications"}
+                icon={<NotificationsIcon />}
+                onClick={openNotification}
+              />
+              <IconBtn
+                title={"Logout"}
+                icon={<LogoutIcon />}
+                onClick={logoutHangler}
+              />
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
+
+const IconBtn = ({ title, icon, onClick }) => {
+  return (
+    <Tooltip title={title}>
+      <IconButton color="inherit" size="large" onClick={onClick}>
+        {icon}
+      </IconButton>
+    </Tooltip>
+  );
 };
 
 export default Header;
