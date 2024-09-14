c0995daadcb8e15ffcb28b7f69a842a453916b9a
ashwani kumar <btechcsesscet@gmail.com>
2024-09-14 10:11:28 +0530

    finishing applayout

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const AppLayout = () => {
-  return <div>AppLayout</div>;
+const AppLayout = () => (WrappedComponent) => {
+  return (props) => {
+    return (
+      <div>
+        <div>Header</div>
+        <WrappedComponent {...props} />
+        <div>Footer</div>
+      </div>
+    );
+  };
 };
 
 export default AppLayout;
