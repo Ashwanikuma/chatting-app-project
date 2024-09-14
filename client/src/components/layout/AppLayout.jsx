9ccad7ea3b8c505454f9ca97b44a13c2ff607e78
ashwani kumar <btechcsesscet@gmail.com>
2024-09-14 11:23:51 +0530

    creating grids in AppLayout

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Title from "../shared/Title";
+import { Grid } from "@mui/material";
 
 const AppLayout = () => (WrappedComponent) => {
   return (props) => {
@@ -8,8 +9,23 @@ const AppLayout = () => (WrappedComponent) => {
       <div>
         <Title />
         <Header />
-        <WrappedComponent {...props} />
-        <div>Footer</div>
+        <Grid container height={"calc(100vh - 4rem)"}>
+          <Grid
+            item
+            sm={4}
+            md={3}
+            sx={{ display: { xs: "none", sm: "block" } }}
+            height={"100%"}
+          >
+            First
+          </Grid>
+          <Grid item xs={4} height={"100%"} bgcolor="primary.main">
+            <WrappedComponent {...props} />
+          </Grid>
+          <Grid item xs={4} height={"100%"}>
+            Third
+          </Grid>
+        </Grid>
       </div>
     );
   };
