b24e51d4086c52d4bc12e5656a159f43505a2a87
ashwani kumar <btechcsesscet@gmail.com>
2024-09-15 09:04:13 +0530

    configuring profile.jsx

---
+++ client/src/components/specific/Profile.jsx
@@ -0,0 +1,58 @@
+import { Avatar, Stack, Typography } from "@mui/material";
+import React from "react";
+import {
+  Face as FaceIcon,
+  AlternateEmail as UserNameIcon,
+  CalendarMonth as CalendarIcon,
+} from "@mui/icons-material";
+
+const Profile = () => {
+  return (
+    <Stack spacing={"2rem"} direction={"column"} alignItems={"center"}>
+      <Avatar
+        sx={{
+          width: 200,
+          height: 200,
+          objectFit: "contain",
+          marginBottom: "1rem",
+          border: "5px solid white",
+        }}
+      />
+      <ProfileCard heading={"bio"} text={"hi, my name is ashwani kumar"} />
+      <ProfileCard
+        heading={"username"}
+        text={"_ashwanikumar18_"}
+        Icon={<UserNameIcon />}
+      />
+      <ProfileCard
+        heading={"name"}
+        text={"ashwani kumar"}
+        Icon={<FaceIcon />}
+      />
+      <ProfileCard
+        heading={"Joined date"}
+        text={"ashwani kumar"}
+        Icon={<CalendarIcon />}
+      />
+    </Stack>
+  );
+};
+const ProfileCard = ({ text, Icon, heading }) => (
+  <Stack
+    direction={"row"}
+    alignItems={"center"}
+    spacing={"1rem"}
+    color={"white"}
+    textAlign={"center"}
+  >
+    {Icon && Icon}
+    <Stack>
+      <Typography variant="body1">{text}</Typography>
+      <Typography color={"gray"} variant="caption">
+        {heading}
+      </Typography>
+    </Stack>
+  </Stack>
+);
+
+export default Profile;
